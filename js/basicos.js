f75fefb791e4bf4f9e9cc9baa139058ebe62925f
Julian Rousseau <julianrousseau@hotmail.com>
2020-11-13 12:41:24 -0300

    Constantes (const)

--- js/basicos.js
+++ js/basicos.js
@@ -27,10 +27,27 @@ console.log("variable musica2 Antes del bloque = ", musica2);
 }
 console.log("variable musica2 Despues del bloque = ", musica2);
 
+console.log("##################  Constantes (const)  ##################");
+/*
+Tipos de datos primitivos:
+Debemos agregarle su valor en el mismo momento que la instanciamos 
+No cambian su tipo de dato, ni su valor en el transcurso de la aplicacion
+ */
+const number = 123.123;
+console.log("Cosntante tipo de dato primitivo =", number);
+/*
+Tipos de datos compuestos:
+podemos instanciarlas y luego agregar su valor 
+podemos agregar, quitar y cambiar valores pero no podemos cambiar el tipo de dato (obj, arrey, etc)
+*/
+const objeto = {
+  nombre: "julian",
+  apellido: "rousseau",
+};
 
+console.log("Constante tipo de dato compuesto inicializado", objeto);
 
+objeto.edad = 23;
+objeto.trabajo = "Software Developer";
 
-
-
-
-
+console.log("Constante tipo de dato compuesto modificado", objeto);
