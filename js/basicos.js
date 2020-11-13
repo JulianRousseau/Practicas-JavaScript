2c3c7ce981916887d0c40694f6bdf4cc28b394f0
Julian Rousseau <julianrousseau@hotmail.com>
2020-11-13 17:50:10 -0300

    Template Strings

--- js/basicos.js
+++ js/basicos.js
@@ -9,23 +9,23 @@ es mejor practica usar let
 console.log("************** var **************");
 var musica = "rock";
 
-console.log("variable musica Antes del bloque = ", musica);
+console.log("variable musica Antes del bloque = " + musica);
 //esto es un bloque   {}
 {
   var musica = "pop";
-  console.log("variable musica Durante del bloque = ", musica);
+  console.log("variable musica Durante del bloque = " + musica);
 }
-console.log("variable musica Despues del bloque = ", musica);
+console.log("variable musica Despues del bloque = " + musica);
 
 console.log("************** let **************");
 let musica2 = "rock";
 
-console.log("variable musica2 Antes del bloque = ", musica2);
+console.log("variable musica2 Antes del bloque = " + musica2);
 {
   let musica2 = "pop";
-  console.log("variable musica2 Durante del bloque = ", musica2);
+  console.log("variable musica2 Durante del bloque = " + musica2);
 }
-console.log("variable musica2 Despues del bloque = ", musica2);
+console.log("variable musica2 Despues del bloque = " + musica2);
 
 console.log("##################  Constantes (const)  ##################");
 /*
@@ -52,9 +52,7 @@ objeto.trabajo = "Software Developer";
 
 console.log("Constante tipo de dato compuesto modificado", objeto);
 
-console.log(
-  "##################  Cadenas de Texto (Strings)  ##################"
-);
+console.log("##################  Cadenas de Texto-Strings  ##################");
 
 let nombre = "hola ";
 //otra forma de crear string, pero es lo mismo (tienen los mismos metodos y propiedades)
@@ -70,3 +68,20 @@ console.log(
   nombre2.includes("un"),
   nombre2.split(" ")
 );
+
+console.log("##################  Template Strings  ##################");
+//concatenacion
+let nombre3 = "Julian";
+let apellido3 = "Rousseau";
+let saludo = "Hola mi nombre es " + nombre3 + " " + apellido3;
+console.log(saludo);
+//Usando template strings (``) debemos usar asentos invertidos (back tick)
+//Nos permiten interpolar datos (dentro de una cadena de texto, el valor dinamico de una variable)
+let saludo2 = `Hola mi nombre es ${nombre3} ${apellido3} , un gusto`;
+console.log(saludo2);
+//Nos permiten escribir cadenas de text con saltos de linea sin problemas
+let ul = `<ul>
+    <li>primavera</li>
+    <li>invierno</li>
+</ul>`;
+console.log(ul);
